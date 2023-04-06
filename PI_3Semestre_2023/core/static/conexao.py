78c65b0afa0067c97f7c002df8acbe61496a8700
Klayvert <klayvert.ryan16@gmail.com>
2023-04-06 00:44:52 -0300

    Adjust db connect credentials and html referer

--- PI_3Semestre_2023/core/static/conexao.py
+++ PI_3Semestre_2023/core/static/conexao.py
@@ -2,5 +2,5 @@ import pymongo
 
 class ConexaoMongoDB():
     my_client = pymongo.MongoClient('mongodb://localhost:27017')
-    db_name = my_client['vendas']
-    collection = db_name['vendas']
+    db_name = my_client['Users']
+    collection = db_name['Credentials']
