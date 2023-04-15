e0a87306501c8c89a4a85b4fb75a8ac5da390323
Klayvert <klayvert.ryan16@gmail.com>
2023-04-15 00:38:36 -0300

    Addeding maps and home for urls

--- PI_3Semestre_2023/core/urls.py
+++ PI_3Semestre_2023/core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('cadastro', views.cadastro, name='cadastro'),
     path('cep', views.cep, name='cep'),
+    path('maps', views.testeMapsAPI, name='maps'),
     path('home', views.home, name='home')
 ]
