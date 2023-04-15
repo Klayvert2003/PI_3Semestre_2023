f5098981bb1ef902c9a393698aeb48acf621dac6
Klayvert Alves <80418636+Klayvert2003@users.noreply.github.com>
2023-04-15 02:18:19 -0300

    Added maps url

--- PI_3Semestre_2023/core/urls.py
+++ PI_3Semestre_2023/core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('cadastro', views.cadastro, name='cadastro'),
     path('cep', views.cep, name='cep'),
+    path('maps', views.MapsAPI, name='maps'),
     path('home', views.home, name='home')
 ]
