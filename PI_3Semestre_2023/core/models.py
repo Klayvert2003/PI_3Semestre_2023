8ccaff8156a8e678244af7cb129b8231bf09c16f
Klayvert <klayvert.ryan16@gmail.com>
2023-04-20 23:23:55 -0300

    Addeding comment Model

--- PI_3Semestre_2023/core/models.py
+++ PI_3Semestre_2023/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# class Address(models.Model):
+#     rua = models.CharField('rua', max_length=100)
+#     bairro = models.CharField('bairro', max_length=100)
+#     cep = models.CharField('cep', max_length=100)
+#     cidade = models.CharField('cidade', max_length=100)
