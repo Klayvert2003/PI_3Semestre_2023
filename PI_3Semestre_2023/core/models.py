adf23604b4a57f1221853fe55cd9284a179b588b
Klayvert <klayvert.ryan16@gmail.com>
2023-04-24 00:53:33 -0300

    Adding model to register address of institutions

--- PI_3Semestre_2023/core/models.py
+++ PI_3Semestre_2023/core/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
-# class Address(models.Model):
-#     rua = models.CharField('rua', max_length=100)
-#     bairro = models.CharField('bairro', max_length=100)
-#     cep = models.CharField('cep', max_length=100)
-#     cidade = models.CharField('cidade', max_length=100)
+class Address(models.Model):
+    nome_instituicao = models.CharField('Nome_Instituição', max_length=100)
+    cnpj = models.IntegerField('CNPJ', max_length=18)
+    cep = models.IntegerField('CEP')
+    rua = models.CharField('Rua', max_length=100)
+    numero = models.IntegerField('Número')
+    bairro = models.CharField('Bairro', max_length=100)
+    cidade = models.CharField('Cidade', max_length=100)
+    complemento = models.CharField('Complemento', max_length=100)
+    estado = models.CharField('Estado', max_length=100)
+    data_hora_cadastro = models.DateTimeField('Data_Hora_Cadastro', auto_now_add=True)
