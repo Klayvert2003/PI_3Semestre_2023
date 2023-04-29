5c9ae0b2ebefb67614260be2ec27fd2d35daa86b
Klayvert <klayvert.ryan16@gmail.com>
2023-04-29 01:28:41 -0300

    Added two new tables in models

--- PI_3Semestre_2023/core/models.py
+++ PI_3Semestre_2023/core/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
-class Address(models.Model):
+class DadosInstituicao(models.Model):
     nome_instituicao = models.CharField('Nome_Instituição', max_length=100)
-    cnpj = models.IntegerField('CNPJ')
-    cep = models.IntegerField('CEP')
+    cep = models.CharField('CEP', max_length=9)
+    cnpj = models.CharField('CNPJ', max_length=14)
     rua = models.CharField('Rua', max_length=100)
-    numero = models.IntegerField('Número')
     bairro = models.CharField('Bairro', max_length=100)
     cidade = models.CharField('Cidade', max_length=100)
-    complemento = models.CharField('Complemento', max_length=100)
-    estado = models.CharField('Estado', max_length=100)
+    estado = models.CharField('Estado', max_length=2)
+    data_hora_cadastro = models.DateTimeField('Data_Hora_Cadastro', auto_now_add=True)
+
+class DadosUsuarios(models.Model):
+    nome_usuario = models.CharField('Nome_Usuário', max_length=100)
+    cep = models.CharField('CEP', max_length=9)
+    rua = models.CharField('Rua', max_length=100)
+    bairro = models.CharField('Bairro', max_length=100)
+    cidade = models.CharField('Cidade', max_length=100)
+    estado = models.CharField('Estado', max_length=2)
     data_hora_cadastro = models.DateTimeField('Data_Hora_Cadastro', auto_now_add=True)
