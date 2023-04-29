9ff1f8b627ca24ecdcd8bc1eee1592b9364a0428
Klayvert <klayvert.ryan16@gmail.com>
2023-04-29 02:26:50 -0300

    Removing unused files

--- PI_3Semestre_2023/database/conexao.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-class ConexaoMongoDB():
-    def __init__(self, locations=False):
-        self.my_client = pymongo.MongoClient('mongodb://localhost:27017')
-        self.db_name = 'Locations' if locations else 'Users'
-        self.collection_name = 'Address' if locations else 'Credentials'
-        self.collection = self.my_client[self.db_name][self.collection_name]
