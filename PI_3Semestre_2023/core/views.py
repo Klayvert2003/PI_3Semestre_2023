b9e0193060ac4db2f5a1a86bc11bc84d7aa44cd6
Klayvert <klayvert.ryan16@gmail.com>
2023-04-29 01:30:28 -0300

    Adding new function that calls GoogleMapsAPI class for zip code validation

--- PI_3Semestre_2023/core/views.py
+++ PI_3Semestre_2023/core/views.py
@@ -1,23 +1,48 @@
-from email.policy import HTTP
-from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as login_django
 from django.contrib.auth.decorators import login_required
-from core.models import Address
 from bs4 import BeautifulSoup
 # My functions
+from core.models import DadosInstituicao, DadosUsuarios
 from api.correiosAPI import BuscaCEP
 from api.GoogleMapsAPI import GoogleMapsAPI
+
+def get_address(cep):
+    address = GoogleMapsAPI
+    data = address.buscar_endereco(address=cep)
+    rua = str(data[0]['formatted_address']).split('-')[0].strip()
+    bairro = str(data[0]['formatted_address']).split('-')[1].split(',')[0].strip()
+    cidade = str(data[0]['formatted_address']).split('-')[1].split(',')[1].strip()
+    estado = str(data[0]['formatted_address']).split('-')[2].split(',')[0].strip()
+
+    return rua, bairro, cidade, estado
+
+def get_cnpj(cnpj):
+    ...
 
 def cadastro_instituicao(request):
     if request.method == 'GET':
         return render(request, 'cadastro-instituicao.html')
     else:   
-        nome_completo = request.POST.get('nome-completo')     
+        nome_instituicao = request.POST.get('nome-completo')     
         cep = request.POST.get('cep')
+        if cep:
+            try:
+                cep = str(cep).replace('-', '').replace('.', '')
+                get_address(cep=cep)
+            except IndexError:
+                return HttpResponse('Insira apenas números!!!')
+            
         cnpj = request.POST.get('cnpj')
+        if cnpj:
+            try:
+                cep = str(cnpj).replace('-', '').replace('.', '')
+                get_cnpj(cep=cep)
+            except IndexError:
+                return HttpResponse('Insira apenas números!!!')
+
         email = request.POST.get('email')
         usuario = request.POST.get('usuario')
         password = request.POST.get('senha')
@@ -25,8 +50,9 @@ def cadastro_instituicao(request):
         if password != confirm_password:
             return HttpResponse('Senhas divergentes, digite a senha idêntica a inserida anteriormente')
 
-        #address = Address.objects.create(nome_instituicao=nome_instituicao, cep=cep, 
-        #rua=rua, numero=numero, bairro=bairro, cidade=cidade, complemento=complemento, estado=estado)
+        dados = DadosInstituicao.objects.create(nome_instituicao=nome_instituicao, cep=cep, cnpj=cnpj, 
+        rua=get_address(cep=cep)[0], bairro=get_address(cep=cep)[1], cidade=get_address(cep=cep)[2], 
+        estado=get_address(cep=cep)[3])
         
         user = User.objects.filter(email=email).first()
         if user:
@@ -44,10 +70,11 @@ def cadastro_usuario(request):
         cep = request.POST.get('cep')
         if cep:
             try:
-                BuscaCEP.buscar_endereco(cep=cep)
-                insere_cep = Address.objects.create()
-            except:
-                return HttpResponse('CEP inválido!!!')
+                cep = str(cep).replace('-', '').replace('.', '')
+                get_address(cep=cep)
+            except IndexError:
+                return HttpResponse('Insira apenas números!!!')
+
         email = request.POST.get('email')
         usuario = request.POST.get('usuario')
         password = request.POST.get('senha')
@@ -55,8 +82,9 @@ def cadastro_usuario(request):
         if password != confirm_password:
             return HttpResponse('Senhas divergentes, digite a senha idêntica a inserida anteriormente')
 
-        #address = Address.objects.create(nome_instituicao=nome_instituicao, cep=cep, 
-        #rua=rua, numero=numero, bairro=bairro, cidade=cidade, complemento=complemento, estado=estado)
+        dados = DadosUsuarios.objects.create(nome_usuario=nome_completo, cep=cep, 
+        rua=get_address(cep=cep)[0], bairro=get_address(cep=cep)[1], cidade=get_address(cep=cep)[2], 
+        estado=get_address(cep=cep)[3])
         
         user = User.objects.filter(email=email).first()
         if user:
@@ -81,51 +109,6 @@ def login(request):
             return render(request, 'home.html')
         else:
             return HttpResponse('Email ou senha inválidos')
-        
-def cep(request):
-    if request.method == 'GET':
-        return render(request, 'cep.html')
-    
-    cep = request.POST.get('cep')
-    try:
-        BuscaCEP.buscar_endereco(cep=cep)
-    except:
-        return HttpResponse('CEP inválido!!!')
-
-    # Se houver conteúdo no campo cep(e ele for válido)é feita a requisição 
-    # então é retornado o html pronto com os dados da API
-    if cep:
-        return render(request, 'novo_cep.html')
-     
-def MapsAPI(request, input_address=''):
-    if request.method == 'GET':
-        return render(request, 'localizacao.html')
-    
-    client = GoogleMapsAPI()
-    input_address = request.POST.get('search')
-
-    try:
-        enderecos = client.buscar_endereco(address=input_address)
-        # rua = str(enderecos[0]['formatted_address']).split('-')[0]
-        # bairro = str(enderecos[0]['formatted_address']).split('-')[1].split(',')[0]
-        # cidade = str(enderecos[0]['formatted_address']).split('-')[1].split(',')[1]
-        # cep = str(enderecos[0]['formatted_address']).split(',')[2]
-
-        # db_addres = Address(rua=rua, bairro=bairro, cidade=cidade, cep=cep)
-        # db_addres.save()
-
-        with open('core/templates/localizacao.html', 'r') as file:
-            html = file.read()
-
-            soup = BeautifulSoup(html, 'html.parser')
-            soup.find('input', {'id': 'search'}).attrs['value'] = enderecos[0]['formatted_address']
-
-        with open('core/templates/nova_localizacao.html', 'w') as file:
-            html = file.write(str(soup.prettify()))
-
-        return render(request, 'nova_localizacao.html')
-    except IndexError:
-        return HttpResponse('Endereço Inválido')
 
 # @login_required(login_url='/auth/login')
 def home(request):
