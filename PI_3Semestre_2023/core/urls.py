5178114ccf667e18b156a1552b0f3afeb74d80fa
Klayvert <klayvert.ryan16@gmail.com>
2023-04-29 01:29:27 -0300

    Removing unused urls

--- PI_3Semestre_2023/core/urls.py
+++ PI_3Semestre_2023/core/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('cadastro-instituicao', views.cadastro_instituicao, name='cadastro-instituicao'),
     path('cadastro-usuario', views.cadastro_usuario, name='cadastro-usuario'),
-    path('cep', views.cep, name='cep'),
-    path('maps', views.MapsAPI, name='maps'),
     path('home', views.home, name='home')
 ]
