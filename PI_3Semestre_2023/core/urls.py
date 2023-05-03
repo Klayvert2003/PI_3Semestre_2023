fe55abb5a850f7e0a9fbb2a1df4d7d01ed9ff2ed
Klayvert2003 <klayvert.ryan16@gmail.com>
2023-05-03 15:21:09 -0300

    Added new URL

--- PI_3Semestre_2023/core/urls.py
+++ PI_3Semestre_2023/core/urls.py
@@ -1,13 +1,12 @@
 from unicodedata import name
 from django.urls import path
 from . import views
-from .views import LoginView
-from .views import CadastroInstituicaoView
-from .views import CadastroUsuarioView
+from .views import LoginView, CadastroInstituicaoView, CadastroUsuarioView, InstituicoesView
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='login'),
     path('cadastro-instituicao', CadastroInstituicaoView.as_view(), name='cadastro-instituicao'),
     path('cadastro-usuario', CadastroUsuarioView.as_view(), name='cadastro-usuario'),
+    path('instituicoes', InstituicoesView.as_view(), name='instituicoes'),
     path('home', views.home, name='home')
 ]
