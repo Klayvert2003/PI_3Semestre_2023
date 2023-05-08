77132b935f6cac97af64df8f0dd055aafac3746a
Klayvert <klayvert.ryan16@gmail.com>
2023-05-08 00:31:14 -0300

    Adjusting return of InstituicoesView class, adding new registration fields to the database of CadastroInstituicaoView and CadastroUsuarioView classes, and adding CardMapView class for the new URL

--- PI_3Semestre_2023/core/views.py
+++ PI_3Semestre_2023/core/views.py
@@ -1,6 +1,6 @@
 from django.views import View
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
@@ -17,10 +17,11 @@ class CadastroInstituicaoView(ValidaCNPJ, GoogleMapsAPI, View):
     def post(self, request): 
         nome_instituicao = request.POST.get('nome-completo')     
         cep = request.POST.get('cep')
-        if cep:
+        num = request.POST.get('num')
+        if cep and num:
             try:
                 cep = str(cep).replace('-', '').replace('.', '')
-                address = self.get_address(cep=cep)
+                address = self.get_complete_address(cep=cep, num=num)
             except IndexError:
                 return HttpResponse('Insira apenas números!!!')
             
@@ -38,9 +39,9 @@ class CadastroInstituicaoView(ValidaCNPJ, GoogleMapsAPI, View):
         if password != confirm_password:
             return HttpResponse('Senhas divergentes, digite a senha idêntica a inserida anteriormente')
 
-        dados = DadosInstituicao.objects.create(nome_instituicao=nome_instituicao, cep=cep, cnpj=cnpj, 
+        dados = DadosInstituicao.objects.create(nome_instituicao=nome_instituicao, cep=cep, num=num, cnpj=cnpj, 
         rua=address[0], bairro=address[1], cidade=address[2], 
-        estado=address[3])
+        estado=address[3], latitude=address[4], longitude=address[5])
         
         user = User.objects.filter(email=email).first()
         if user:
@@ -73,7 +74,7 @@ class CadastroUsuarioView(GoogleMapsAPI, View):
 
         dados = DadosUsuarios.objects.create(nome_usuario=nome_completo, cep=cep, 
         rua=address[0], bairro=address[1], cidade=address[2], 
-        estado=address[3], latitude=address[4], longitude=address[5])
+        estado=address[3])
         
         user = User.objects.filter(email=email).first()
         if user:
@@ -102,8 +103,26 @@ class LoginView(View):
         
 class InstituicoesView(GoogleMapsAPI, View):
     def get(self, request):
-        dados = DadosInstituicao.objects.all()
+        dados = list(DadosInstituicao.objects.all().values('id', 'nome_instituicao', 'latitude', 'longitude'))
+
         return render(request, 'instituicoes.html', {'dados': dados})
+
+    def post(self):
+        return redirect('card-map')
+
+class CardMapView(View):
+    def get(self, request):
+        lat = request.GET.get('latitude', None)
+        lon = request.GET.get('longitude', None)
+
+        if lat and lon:
+            contexto = {
+                'latitude': lat,
+                'longitude': lon
+            }
+            return render(request, 'card-map.html', contexto)
+        else:
+            return redirect('instituicoes')
 
 class HomeUsuarios(TemplateView):
     # @login_required(login_url='/auth/login')
