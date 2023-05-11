5215364b86ce202e4514a2b95ae511070b4837a6
Klayvert2003 <klayvert.ryan16@gmail.com>
2023-05-11 17:17:35 -0300

    Adding js interaction for mismatched passwords on registration

---
+++ PI_3Semestre_2023/core/static/js/valida_senha.js
@@ -0,0 +1,15 @@
+var senhaInput = document.getElementById('senha');
+var confirmarSenhaInput = document.getElementById('confirma-senha');
+var submitButton = document.getElementById('submit-button');
+
+confirmarSenhaInput.addEventListener('input', function() {
+  if (confirmarSenhaInput.value === senhaInput.value) {
+    submitButton.disabled = false;
+    senhaInput.style.color = 'white';
+    confirmarSenhaInput.style.color = 'white';
+  } else {
+    submitButton.disabled = true;
+    senhaInput.style.color = 'red';
+    confirmarSenhaInput.style.color = 'red';
+  }
+});
