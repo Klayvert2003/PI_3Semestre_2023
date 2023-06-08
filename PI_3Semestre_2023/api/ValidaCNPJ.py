671a167b8d2019b0c49b712446e2fee0457d974f
Klayvert <klayvert.ryan16@gmail.com>
2023-06-08 17:37:11 -0300

    Leveraging more data from the API

--- PI_3Semestre_2023/api/ValidaCNPJ.py
+++ PI_3Semestre_2023/api/ValidaCNPJ.py
@@ -6,5 +6,7 @@ class ValidaCNPJ():
         response = requests.get(url).json()
         cep = str(response['cep']).replace('.', '').replace('-', '')
         numero = response['numero']
+        complemento = response['complemento']
+        telefone = str(response['telefone']).split('/')[0]
 
-        return cep, numero
+        return cep, numero, complemento, telefone
