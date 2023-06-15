5468e5aa5f425314f2997979abe92e47e8afa50b
Klayvert Alves <klayvert.ryan16@gmail.com>
2023-06-14 23:35:26 -0300

    Adjust return values on BuscaCNPJ

--- PI_3Semestre_2023/api/ValidaCNPJ.py
+++ PI_3Semestre_2023/api/ValidaCNPJ.py
@@ -4,9 +4,12 @@ class ValidaCNPJ():
     def BuscaCNPJ(self, cnpj: str) -> str:
         url = f'https://receitaws.com.br/v1/cnpj/{cnpj}'
         response = requests.get(url).json()
-        cep = str(response['cep']).replace('.', '').replace('-', '')
-        numero = response['numero']
-        complemento = response['complemento']
-        telefone = str(response['telefone']).split('/')[0]
-
-        return cep, numero, complemento, telefone
+        try:
+            cep = str(response['cep']).replace('.', '').replace('-', '')
+            numero = response['numero']
+            complemento = response['complemento']
+            telefone = str(response['telefone']).split('/')[0]
+            
+            return cep, numero, complemento, telefone
+        except:
+            return False
