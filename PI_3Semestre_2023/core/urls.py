3031471661200024b0d1f04c44cbd8d1a3b62c19
Felipe Vieira <felipedpvs@hotmail.com>
2023-06-17 19:41:23 -0300

    editando o caminho na path

--- PI_3Semestre_2023/core/urls.py
+++ PI_3Semestre_2023/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ListaContato, DeletarUsuario, EditarUsuario, LoginView, LogoutView, CadastroInstituicaoView, CadastroUsuarioView, InstituicoesView, HomeUsuarios, HomeInstituicao, CardMapView, Index, DetalhesInstituicao, DetalhesUsuario, MenuUsuario, ContatoView, SobreView
+from .views import ListaContato, DeletarUsuario, EditarUsuario, LoginView, LogoutView, CadastroInstituicaoView, CadastroUsuarioView, InstituicoesView, HomeUsuarios, HomeInstituicao, CardMapView, Index, DetalhesInstituicao, DetalhesUsuario, ContatoView, SobreView
 urlpatterns = [
     path('index', Index.as_view(), name='index'),
     path('login', LoginView.as_view(), name='login'),
@@ -13,7 +13,6 @@ urlpatterns = [
     path('detalhe-instituicao', DetalhesInstituicao.as_view(), name='detalhe-instituicao'),
     path('detalhe-usuario', DetalhesUsuario.as_view(), name='detalhe-usuario'),
     path('deletar-usuario', DeletarUsuario.as_view(), name='deletar-usuario'),
-    path('menu-usuario', MenuUsuario.as_view(), name='menu-usuario'),
     path('editar-usuario', EditarUsuario.as_view(), name='editar-usuario'),
     path('contato', ContatoView.as_view(), name='contato'),
     path('sobre', SobreView.as_view(), name='sobre'),
