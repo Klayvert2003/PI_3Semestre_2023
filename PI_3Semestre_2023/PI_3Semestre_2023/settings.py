45bbcd37b884c609b818f68e9517c16ba5440251
Klayvert Alves <klayvert.ryan16@gmail.com>
2023-06-19 20:08:20 -0300

    ADD: Adding settings for sending emails.

--- PI_3Semestre_2023/PI_3Semestre_2023/settings.py
+++ PI_3Semestre_2023/PI_3Semestre_2023/settings.py
@@ -123,3 +123,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Envio de email com Django
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
